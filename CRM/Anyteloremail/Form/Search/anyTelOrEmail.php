8d16b37978080b5ff8a942c475375ef3d56cbcf1
JoeMurray <joe.murray@jmaconsulting.biz>
2014-04-27 21:35:43 -0400

    Major rewrite
    Added a group by to get one row per contact, fixed the regex for phone numbers so it works, removed duplicate phones and emails from results, etc.

--- CRM/Anyteloremail/Form/Search/anyTelOrEmail.php
+++ CRM/Anyteloremail/Form/Search/anyTelOrEmail.php
@@ -1,19 +1,19 @@
 <?php
 
 /**
- * A custom contact search
+ * A custom contact search that returns one row for each contact with a partial or full match on any of its emails or phone numbers
  */
 class CRM_Anyteloremail_Form_Search_anyTelOrEmail extends CRM_Contact_Form_Search_Custom_Base implements CRM_Contact_Form_Search_Interface {
+
   function __construct(&$formValues) {
     parent::__construct($formValues);
     $this->_columns = array(
       ts('Contact Id') => 'contact_id',
       ts('Name') => 'sort_name',
-      ts('Email') => 'email',
-      ts('Phone Number') => 'number',
-      ts('Phone Extension') => 'extension',
-      ts('City') => 'city',
-      ts('State') => 'state',
+      ts('Email(s)') => 'email',
+      ts('Phone Number(s)') => 'number',
+      ts('City (Cities)') => 'city',
+      ts('State(s)') => 'state',
     );
   }
 
@@ -31,7 +31,7 @@ class CRM_Anyteloremail_Form_Search_anyTelOrEmail extends CRM_Contact_Form_Searc
     /**
      * You can define a custom title for the search form
      */
-    $this->setTitle('Partial Phone Number and Email Search');
+    $this->setTitle('Any Telephone or Email Search');
 
     $form->addElement('header', "howdy", 'QuickForm tutorial example');
     /**
@@ -47,14 +47,18 @@ class CRM_Anyteloremail_Form_Search_anyTelOrEmail extends CRM_Contact_Form_Searc
   }
 
   function all($offset = 0, $rowcount = 0, $sort = NULL, $includeContactIDs = FALSE, $justIDs = FALSE) {
-    $selectClause = "contact_a.id AS contact_id, contact_a.sort_name as sort_name, ".
-     "(SELECT GROUP_CONCAT(email SEPARATOR ', ') FROM civicrm_email WHERE contact_id = contact_a.id) AS email, ".
-     "(SELECT GROUP_CONCAT(phone SEPARATOR ', ') FROM civicrm_phone WHERE contact_id = contact_a.id) AS number, ".
-     "phone_ext AS extension, city AS city, sp.name AS state ";
-    $all = $this->sql($selectClause,
+    $partialSelect = "contact_a.id AS contact_id, contact_a.sort_name as sort_name, ".
+     "(SELECT GROUP_CONCAT(DISTINCT email SEPARATOR ', ') FROM civicrm_email WHERE contact_id = contact_a.id) AS  email, ".
+     "(SELECT GROUP_CONCAT(DISTINCT CONCAT(phone,IF(phone_ext IS NULL,'',CONCAT(' ',phone_ext))) SEPARATOR ', ') FROM civicrm_phone WHERE contact_id = contact_a.id) AS number, ".
+     "GROUP_CONCAT(DISTINCT city SEPARATOR ', ') AS city, ".
+     "GROUP_CONCAT(DISTINCT sp.name SEPARATOR ', ') AS state ";
+    $groupBy = $this->groupBy();
+
+    $all = $this->sql($partialSelect,
       $offset, $rowcount, $sort,
-      $includeContactIDs, NULL
+      $includeContactIDs, $groupBy
     );
+
     return $all;
   }
 
@@ -67,40 +71,46 @@ class CRM_Anyteloremail_Form_Search_anyTelOrEmail extends CRM_Contact_Form_Searc
       "LEFT JOIN civicrm_state_province sp ON ".
       "(address.state_province_id = sp.id) ".
       "LEFT JOIN civicrm_email email ON ".
-      "(contact_a.id = email.contact_id)";
+      "(contact_a.id = email.contact_id) ";
   }
 
   function where($includeContactIDs = FALSE) {
+
     $params = array();
-
-    $phone = CRM_Utils_Array::value('number',
-      $this->_formValues
-    );
-    $email = CRM_Utils_Array::value('email',
-      $this->_formValues
-    );
-
     $where = array();
-    if(!empty($phone)) {
-      $phone = $this->convertNumberToRegex($phone);
-      $where[] = "(contact_a.is_deleted = 0 AND phone.phone REGEXP %1)";
-      $params[1] = array($phone, 'String');
-    }
-    if(!empty($email)) {
-      $where[] = "(contact_a.is_deleted = 0 AND email.email REGEXP %2)";
-      $params[2] = array('.*' . $email . '.*', 'String');
-    }
-    if (!empty($phone) && !empty($email)) {
-      $where = implode(' AND ', $where);
-    }
-    else {
-      $where = implode(' OR ', $where);
-    }
-    return $this->whereClause($where, $params);
+
+    $phone = CRM_Utils_Array::value('number', $this->_formValues);
+    $phoneregex = $this->convertNumberToRegex($phone);
+    $email = CRM_Utils_Array::value('email', $this->_formValues);
+
+    if(!empty($phone) && !empty($email)) {
+      $where = "(contact_a.is_deleted = 0 AND phone.phone REGEXP %1) AND (contact_a.is_deleted = 0 AND email.email REGEXP %2) ";
+      $params[1] = array($phoneregex, 'String');
+      $params[2] = array($email, 'String');
+    } elseif(!empty($email)) { // $phone is empty
+      $where = "(contact_a.is_deleted = 0 AND email.email REGEXP %1) ";
+      $params[1] = array('.*' . $email . '.*', 'String');
+    } elseif (!empty($phone)) { // $email is empty
+      $where = "(contact_a.is_deleted = 0 AND phone.phone REGEXP %1) ";
+      $params[1] = array($phoneregex, 'String');
+    } // else both are empty
+
+    $whereClause = $this->whereClause($where, $params);
+
+    return $whereClause;
   }
 
   function convertNumberToRegex($number) {
-    // We assume US style number with 10 digits
+    // search for North American phone numbers of form (416) 444-4444
+    // and allow any form of extension to follow it
+    // the following are all valid
+    // 4444444444
+    // 444 444-4444
+    // (444)444-4444
+    // (444) 444-4444
+    // 444.444.4444
+    // (444) 444-4444 x123
+    // (444) 444-4444 ext. 123
 
     // First remove everything that is not a number
     $number = preg_replace('/[^0-9]/', '', $number);
@@ -110,9 +120,20 @@ class CRM_Anyteloremail_Form_Search_anyTelOrEmail extends CRM_Contact_Form_Searc
     $prefix = substr($number,3,3);
     $last = substr($number,6,4);
 
-    return "[^0-9]*$area_code[^0-9]*$prefix[^0-9]*$last[^0-9]*";
+    // now build it up
+    $nnstr = "[^0-9]*"; // non-numeric string of 0 to n characters
+    // The ^ forces match to start at beginning of string; anything can follow the number eg extensions of various forms
+    $str = '^' . $nnstr . $area_code . $nnstr . $prefix . $nnstr . $last;
+
+    return $str;
 
   }
+
+  function groupBy() {
+    $groupBy = "GROUP BY contact_id ";
+    return $groupBy;
+  }
+
   function setDefaultValues() {
     return array();
   }
